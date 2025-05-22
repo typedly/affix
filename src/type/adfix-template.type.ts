644ea9c6b74a9b8376398ced8990df2d2ef08d4a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-22 22:26:14 +0000

    refactor(`AffixTemplate`): change the name to `AdfixTemplate`.

---
+++ src/type/adfix-template.type.ts
@@ -0,0 +1,14 @@
+/**
+ * @description The type defines the basic structure of adfix template where the prefix, stem, and suffix are separated by a delimiter.
+ * @export
+ * @template {string} Prefix The prefix part of the adfix template.
+ * @template {string} Stem The stem part of the adfix template.
+ * @template {string} Suffix The suffix part of the adfix template.
+ * @template {string} Delimiter The delimiter used to separate the prefix, stem, and suffix.
+ */
+export type AdfixTemplate<
+  Prefix extends string,
+  Stem extends string,
+  Suffix extends string,
+  Delimiter extends string
+> = `${Prefix}${Delimiter}${Stem}${Delimiter}${Suffix}`;
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,6 +1,6 @@
+export type { AdfixTemplate } from './adfix-template.type';
 export type { AffixKind } from './affix-kind.type';
-export type { AffixTemplate } from './affix-template.type';
 export type { AffixTuple } from './affix-tuple.type';
 export type { BasicAffixKind } from './basic-affix-kind.type';
-export type { SuprasegmentalAffixKind } from './suprasegmental-affix-kind.type';
 export type { HypotheticalAffixKind } from './hypothetical-affix-kind.type';
+export type { SuprasegmentalAffixKind } from './suprasegmental-affix-kind.type';
