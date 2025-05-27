41ab605b42c867397ab73baa07e187b4aacf7bd2
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-27 16:13:14 +0000

    test: add `AffixConfiguration` and `AffixSettings`.

---
+++ src/test/affix-configuration.spec.ts
@@ -0,0 +1,26 @@
+import { AffixConfiguration } from '../interface';
+
+// Length and pattern exact value.
+const affixConfiguration1: AffixConfiguration<'example', 7> = {
+  value: 'example',
+  length: 7,
+  pattern: /^[a-zA-Z]+$/,
+  timestamp: new Date(),
+};
+
+// Full configuration with length and pattern settings.
+const affixConfiguration2: AffixConfiguration<'example', 7, 27, 34> = {
+  value: 'example',
+  length: {
+    value: 7,
+    min: 27,
+    max: 34 
+  },
+  pattern: {
+    regexp: /^[a-zA-Z]+$/,
+    lowercase: true,
+    uppercase: true,
+    special: true,
+  },
+  timestamp: new Date(),
+};
