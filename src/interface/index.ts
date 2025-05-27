1367adfeb1d9e2b4e921060b2e3e3c9ed4399dd2
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-27 16:20:19 +0000

    refactor(`AffixOptions`): use `AffixSettings` for options.

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,3 +1,4 @@
 export type { AffixConfiguration } from './affix-configuration.interface';
 export type { AffixConstructor } from './affix-constructor.interface';
 export type { AffixOptions } from './affix-options.interface';
+export type { AffixSettings } from './affix-settings.interface';
