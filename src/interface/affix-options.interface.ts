3a765becb6d9e1665060da9283bcbc6c2bc87e48
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-01 05:06:05 +0000

    refactor: add `Kind`.

--- src/interface/affix-options.interface.ts
+++ src/interface/affix-options.interface.ts
@@ -1,15 +1,31 @@
 // Interface.
 import { AffixSettings } from "./affix-settings.interface";
+// Type.
+import { BasicAffixKind } from "../type";
 /**
  * @description The interface extends the `AffixConfiguration` interface and allows for partial configuration.
  * @export
  * @interface AffixOptions
- * @template {string} Value The type of the value property, defaults to `string`.
+ * @template {string | undefined} [Value=string] The type of the value property, defaults to `string`.
+ * @template {number | undefined} [Length=number | undefined] The type of the length property, defaults to `number | undefined`.
+ * @template {number | undefined} [Min=number | undefined] The type of the min property, defaults to `number | undefined`.
+ * @template {number | undefined} [Max=number | undefined] The type of the max property, defaults to `number | undefined`.
+ * @template {BasicAffixKind | undefined} [Kind=BasicAffixKind | undefined] The type of the kind property, defaults to `BasicAffixKind`.
+ * @template {RegExp | string | undefined} [Pattern=RegExp | string | undefined] The type of the pattern property, defaults to `RegExp | string | undefined`.
+ * @extends {Partial<AffixSettings<Value, Kind, Pattern>>}
  */
 export interface AffixOptions<
-  Value extends string = string,
-  Length extends number = number,
-  Min extends number = number,
-  Max extends number = number,
-  Pattern extends RegExp = RegExp
-> extends Partial<AffixSettings<Value, Length, Min, Max, Pattern>> {}
+  Value extends string | undefined = string,
+  Kind extends BasicAffixKind | undefined = BasicAffixKind | undefined,
+  Length extends number | undefined = number | undefined,
+  Min extends number | undefined = number | undefined,
+  Max extends number | undefined = number | undefined,
+  Pattern extends RegExp | string | undefined = RegExp | string | undefined
+> extends Partial<AffixSettings<
+  Value,
+  Kind,
+  Length,
+  Min,
+  Max,
+  Pattern
+>> {}
