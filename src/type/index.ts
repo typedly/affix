eb49e7c1b0d502a456ff4497012e9a5279e7e226
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-08 07:40:01 +0000

    chore(type/index): update.

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,7 +1,14 @@
 export type { Adfix } from './adfix.type';
-export type { AdfixTemplate } from './adfix-template.type';
 export type { AdfixTuple } from './adfix-tuple.type';
 export type { AffixKind } from './affix-kind.type';
 export type { BasicAffixKind } from './basic-affix-kind.type';
 export type { HypotheticalAffixKind } from './hypothetical-affix-kind.type';
 export type { SuprasegmentalAffixKind } from './suprasegmental-affix-kind.type';
+
+// Template.
+export type { AdfixTemplate } from './adfix-template.type';
+export type { CircumfixTemplate } from './circumfix-template.type';
+export type { InfixTemplate } from './infix-template.type';
+export type { InterfixTemplate } from './interfix-template.type';
+export type { PrefixTemplate } from './prefix-template.type';
+export type { SuffixTemplate } from './suffix-template.type';
